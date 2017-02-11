4d7f85c3ee86e0ce17938f70f0a72701c5d6c09d
Angel Stoyanov <atstojanov@gmail.com>
2017-02-11 11:47:01 +0200

    Format using VSCode

--- AccelerationGyroMeasurement.ino
+++ AccelerationGyroMeasurement.ino
@@ -15,14 +15,13 @@ const byte ROWS = 2; //Два реда
 const byte COLS = 2; //Две колони
 
 char keys[ROWS][COLS] = {
-  {'4', '3'},
-  {'2', '1'}
-};
+    {'4', '3'},
+    {'2', '1'}};
 
-byte rowPins[ROWS] = {4, 5};                                                          //пинове към които са свързани редовете на клавиатурата
-byte colPins[COLS] = {6, 7};                                                          //пинове към които са свързани колоните на клавиатурата
+byte rowPins[ROWS] = {4, 5}; //пинове към които са свързани редовете на клавиатурата
+byte colPins[COLS] = {6, 7}; //пинове към които са свързани колоните на клавиатурата
 
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 int displayInterval = 500;
 
@@ -61,30 +60,32 @@ float angle_pitch_output, angle_roll_output;
 #define MENU_HOME 1
 #define MENU_HOME_MAX 2
 
-#define MPU6050_ADDRESS             0x68
+#define MPU6050_ADDRESS 0x68
 
-#define MPU6050_RA_GYRO_CONFIG      0x1B
-#define MPU6050_RA_ACCEL_CONFIG     0x1C
+#define MPU6050_RA_GYRO_CONFIG 0x1B
+#define MPU6050_RA_ACCEL_CONFIG 0x1C
 
-#define MPU6050_GYRO_FS_250         0x00
-#define MPU6050_GYRO_FS_500         0x01
-#define MPU6050_GYRO_FS_1000        0x02
-#define MPU6050_GYRO_FS_2000        0x03
+#define MPU6050_GYRO_FS_250 0x00
+#define MPU6050_GYRO_FS_500 0x01
+#define MPU6050_GYRO_FS_1000 0x02
+#define MPU6050_GYRO_FS_2000 0x03
 
-#define MPU6050_ACCEL_FS_2          0x00
-#define MPU6050_ACCEL_FS_4          0x01
-#define MPU6050_ACCEL_FS_8          0x02
-#define MPU6050_ACCEL_FS_16         0x03
+#define MPU6050_ACCEL_FS_2 0x00
+#define MPU6050_ACCEL_FS_4 0x01
+#define MPU6050_ACCEL_FS_8 0x02
+#define MPU6050_ACCEL_FS_16 0x03
 
-#define UPPER_RANGE 24576 // 75% of int16_t
+#define UPPER_RANGE 24576  // 75% of int16_t
 #define BOTTOM_RANGE 16384 // 50% of int16_t
 
 volatile bool refreshNeeded = false;
 volatile byte buttonPressed = 0;
+volatile bool setGyroAngles = false;
 
 int menuPosition = MENU_HOME;
 
-void interrupt() {
+void interrupt()
+{
   refreshNeeded = true;
 }
 
@@ -92,50 +93,50 @@ int accRange, gyroRange;
 
 void setup()
 {
-  display.begin();                                                                    // Стартиране на връзката с дисплея.
+  display.begin(); // Стартиране на връзката с дисплея.
   display.setFont(SmallFont);
 
-  Wire.begin();                                                                       //Стартиране на I2C в режим мастър
+  Wire.begin(); //Стартиране на I2C в режим мастър
 
-  Serial.begin(38400);                                                                //Стартиране на серийната комуникация. Скорост 38400 bps
+  Serial.begin(38400); //Стартиране на серийната комуникация. Скорост 38400 bps
   Serial.println("Setting some things up.");
 
   MsTimer2::set(100, interrupt);
   MsTimer2::start();
 
-  pinMode(LED_PIN, OUTPUT);                                                           // LED пина конфигуриран, като изход.
+  pinMode(LED_PIN, OUTPUT); // LED пина конфигуриран, като изход.
 
-  setup_mpu_6050_registers();                                                         //Конфигуриране на mpu_6050.
-  
+  setup_mpu_6050_registers(); //Конфигуриране на mpu_6050.
+
   // calibrateGyro();
 
   Serial.println("Done");
 }
 
-void loop() {
+void loop()
+{
 
   buttonPressed = keypad.getKey();
-  switch (buttonPressed) {
-    case BUTTON_1:
-      reset();
-      break;
-    case BUTTON_3:
-      if (menuPosition == MENU_HOME)
-        menuPosition = MENU_HOME_MAX;
-      else
-        menuPosition = MENU_HOME;
-      break;
+  switch (buttonPressed)
+  {
+  case BUTTON_1:
+    reset();
+    break;
+  case BUTTON_3:
+    if (menuPosition == MENU_HOME)
+      menuPosition = MENU_HOME_MAX;
+    else
+      menuPosition = MENU_HOME;
+    break;
   }
-
-
 
   //  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
   read_mpu_6050_data();
 
-  gx -= gyro_x_cal;                                                //Subtract the offset calibration value from the raw gyro_x value
-  gy -= gyro_y_cal;                                                //Subtract the offset calibration value from the raw gyro_y value
-  gz -= gyro_z_cal;                                                //Subtract the offset calibration value from the raw gyro_z value
+  gx -= gyro_x_cal; //Subtract the offset calibration value from the raw gyro_x value
+  gy -= gyro_y_cal; //Subtract the offset calibration value from the raw gyro_y value
+  gz -= gyro_z_cal; //Subtract the offset calibration value from the raw gyro_z value
 
   //  compare(ax, &maxax);
   //  compare(ay, &maxay);
@@ -149,56 +150,62 @@ void loop() {
   calculateAngles();
 
   refresh();
-  digitalWrite(LED_PIN, !digitalRead(LED_PIN));;
+  digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+  ;
 }
 
-void read_mpu_6050_data() {                                            //Subroutine for reading the raw gyro and accelerometer data
-  Wire.beginTransmission(MPU6050_ADDRESS);                                        //Start communicating with the MPU-6050
-  Wire.write(0x3B);                                                    //Send the requested starting register
-  Wire.endTransmission();                                              //End the transmission
-  Wire.requestFrom(MPU6050_ADDRESS, 14);                                          //Request 14 bytes from the MPU-6050
-  while (Wire.available() < 14);                                       //Wait until all the bytes are received
-  ax = Wire.read() << 8 | Wire.read();                                 //Add the low and high byte to the acc_x variable
-  ay = Wire.read() << 8 | Wire.read();                                 //Add the low and high byte to the acc_y variable
-  az = Wire.read() << 8 | Wire.read();                                 //Add the low and high byte to the acc_z variable
-  temperature = Wire.read() << 8 | Wire.read();                        //Add the low and high byte to the temperature variable
-  gx = Wire.read() << 8 | Wire.read();                                 //Add the low and high byte to the gyro_x variable
-  gy = Wire.read() << 8 | Wire.read();                                 //Add the low and high byte to the gyro_y variable
-  gz = Wire.read() << 8 | Wire.read();                                //Add the low and high byte to the gyro_z variable
-
+void read_mpu_6050_data()
+{                                          //Subroutine for reading the raw gyro and accelerometer data
+  Wire.beginTransmission(MPU6050_ADDRESS); //Start communicating with the MPU-6050
+  Wire.write(0x3B);                        //Send the requested starting register
+  Wire.endTransmission();                  //End the transmission
+  Wire.requestFrom(MPU6050_ADDRESS, 14);   //Request 14 bytes from the MPU-6050
+  while (Wire.available() < 14)
+    ;                                           //Wait until all the bytes are received
+  ax = Wire.read() << 8 | Wire.read();          //Add the low and high byte to the acc_x variable
+  ay = Wire.read() << 8 | Wire.read();          //Add the low and high byte to the acc_y variable
+  az = Wire.read() << 8 | Wire.read();          //Add the low and high byte to the acc_z variable
+  temperature = Wire.read() << 8 | Wire.read(); //Add the low and high byte to the temperature variable
+  gx = Wire.read() << 8 | Wire.read();          //Add the low and high byte to the gyro_x variable
+  gy = Wire.read() << 8 | Wire.read();          //Add the low and high byte to the gyro_y variable
+  gz = Wire.read() << 8 | Wire.read();          //Add the low and high byte to the gyro_z variable
 }
 
-void setFullScaleAccRange(uint8_t range) {
-  Wire.beginTransmission(MPU6050_ADDRESS);                                        //Start communicating with the MPU-6050
-  Wire.write(0x1C);                                                    //Send the requested starting register
-  Wire.write(range << 3);                                                  //Set the requested starting register
-  Wire.endTransmission();                                              //End the transmission
+void setFullScaleAccRange(uint8_t range)
+{
+  Wire.beginTransmission(MPU6050_ADDRESS); //Start communicating with the MPU-6050
+  Wire.write(0x1C);                        //Send the requested starting register
+  Wire.write(range << 3);                  //Set the requested starting register
+  Wire.endTransmission();                  //End the transmission
 }
 
-void setFullScaleGyroRange(uint8_t range) {
-  Wire.beginTransmission(MPU6050_ADDRESS);                                        //Start communicating with the MPU-6050
-  Wire.write(0x1B);                                                    //Send the requested starting register
-  Wire.write(range << 3);                                                  //Set the requested starting register
-  Wire.endTransmission();                                              //End the transmission
+void setFullScaleGyroRange(uint8_t range)
+{
+  Wire.beginTransmission(MPU6050_ADDRESS); //Start communicating with the MPU-6050
+  Wire.write(0x1B);                        //Send the requested starting register
+  Wire.write(range << 3);                  //Set the requested starting register
+  Wire.endTransmission();                  //End the transmission
 }
 
-void setup_mpu_6050_registers() {
+void setup_mpu_6050_registers()
+{
   //Activate the MPU-6050
-  Wire.beginTransmission(MPU6050_ADDRESS);                                        //Start communicating with the MPU-6050
-  Wire.write(0x6B);                                                    //Send the requested starting register
-  Wire.write(0x01);                                                    //Set the requested starting register
-  Wire.endTransmission();                                              //End the transmission
+  Wire.beginTransmission(MPU6050_ADDRESS); //Start communicating with the MPU-6050
+  Wire.write(0x6B);                        //Send the requested starting register
+  Wire.write(0x01);                        //Set the requested starting register
+  Wire.endTransmission();                  //End the transmission
   setFullScaleAccRange(0);
   setFullScaleGyroRange(0);
 }
 
-void compare(int16_t current, int16_t* maxvalue)
+void compare(int16_t current, int16_t *maxvalue)
 {
   if (abs(current) > abs(*maxvalue))
     *maxvalue = current;
 }
 
-void reset() {
+void reset()
+{
   maxax = maxay = maxaz = maxgx = maxgy = maxgz = 0;
   accRange = 0;
   gyroRange = 0;
@@ -260,7 +267,7 @@ void checkRange()
 int16_t int_pow(int16_t base, int16_t exp)
 {
   int result = base;
-  for (int i = 1; i < exp;  i++)
+  for (int i = 1; i < exp; i++)
   {
     result *= base;
   }
@@ -283,7 +290,7 @@ void displayReadings(int16_t _ax, int16_t _ay, int16_t _az, int16_t _gx, int16_t
   display.print("Angles", colPos(9), rowPos(2));
   display.print("roll=", colPos(9), rowPos(3));
   display.print(angle_roll < 0 ? "-" : "", colPos(15), rowPos(3));
-  display.printNumF(abs(angle_roll), 2, colPos(16) , rowPos(3));
+  display.printNumF(abs(angle_roll), 2, colPos(16), rowPos(3));
   display.print("pitch=", colPos(9), rowPos(4));
   display.print(angle_pitch < 0 ? "-" : "", colPos(15), rowPos(4));
   display.printNumF(abs(angle_pitch), 2, colPos(16), rowPos(4));
@@ -292,11 +299,13 @@ void displayReadings(int16_t _ax, int16_t _ay, int16_t _az, int16_t _gx, int16_t
   display.printNumF(abs(angle_yaw), 2, colPos(16), rowPos(5));
 }
 
-int rowPos(int row) {
+int rowPos(int row)
+{
   return row * FONT_HEIGHT;
 }
 
-int colPos(int col) {
+int colPos(int col)
+{
   return col * FONT_WIDTH;
 }
 
@@ -310,29 +319,33 @@ void displayButtons()
   display.print("Rst", 97, 63 - 8);
 }
 
-void calibrateGyro() {
+void calibrateGyro()
+{
 
   display.clrScr();
   display.print("Calibrating gyro", 0, 0);
   display.update();
 
-  for (int cal_int = 0; cal_int < 2000 ; cal_int ++) {                 //Run this code 2000 times
+  for (int cal_int = 0; cal_int < 2000; cal_int++)
+  { //Run this code 2000 times
     //if(cal_int % 125 == 0) display.print();                              //Print a dot on the LCD every 125 readings
-    read_mpu_6050_data();                                              //Read the raw acc and gyro data from the MPU-6050
-    gyro_x_cal += gx;                                                  //Add the gyro x-axis offset to the gyro_x_cal variable
-    gyro_y_cal += gy;                                                  //Add the gyro y-axis offset to the gyro_y_cal variable
-    gyro_z_cal += gz;                                                  //Add the gyro z-axis offset to the gyro_z_cal variable
-    delay(3);                                                          //Delay 3us to simulate the 250Hz program loop
+    read_mpu_6050_data(); //Read the raw acc and gyro data from the MPU-6050
+    gyro_x_cal += gx;     //Add the gyro x-axis offset to the gyro_x_cal variable
+    gyro_y_cal += gy;     //Add the gyro y-axis offset to the gyro_y_cal variable
+    gyro_z_cal += gz;     //Add the gyro z-axis offset to the gyro_z_cal variable
+    delay(3);             //Delay 3us to simulate the 250Hz program loop
   }
 
-  gyro_x_cal /= 2000;                                                  //Divide the gyro_x_cal variable by 2000 to get the avarage offset
-  gyro_y_cal /= 2000;                                                  //Divide the gyro_y_cal variable by 2000 to get the avarage offset
-  gyro_z_cal /= 2000;                                                  //Divide the gyro_z_cal variable by 2000 to get the avarage offset
+  gyro_x_cal /= 2000; //Divide the gyro_x_cal variable by 2000 to get the avarage offset
+  gyro_y_cal /= 2000; //Divide the gyro_y_cal variable by 2000 to get the avarage offset
+  gyro_z_cal /= 2000; //Divide the gyro_z_cal variable by 2000 to get the avarage offset
 }
 
-void refresh() {
+void refresh()
+{
 
-  if (!refreshNeeded) return;
+  if (!refreshNeeded)
+    return;
 
   refreshNeeded = false;
 
@@ -347,38 +360,39 @@ void refresh() {
 
   switch (menuPosition)
   {
-    case MENU_HOME:
-      displayReadings(ax, ay, az, gx, gy, gz);
-      display.print("MPU6050", CENTER, 0);
-      display.print("Mode", 33, 63 - 8);
-      break;
-    case MENU_HOME_MAX:
-      displayReadings(maxax, maxay, maxaz, maxgx, maxgy, maxgz);
-      display.print("ADXL345", CENTER, 0);
-      display.print("Mode", 33, 63 - 8);
-      break;
+  case MENU_HOME:
+    displayReadings(ax, ay, az, gx, gy, gz);
+    display.print("MPU6050", CENTER, 0);
+    display.print("Mode", 33, 63 - 8);
+    break;
+  case MENU_HOME_MAX:
+    displayReadings(maxax, maxay, maxaz, maxgx, maxgy, maxgz);
+    display.print("ADXL345", CENTER, 0);
+    display.print("Mode", 33, 63 - 8);
+    break;
   }
   display.update();
 }
 
-double convertAcc(int16_t value) {
+double convertAcc(int16_t value)
+{
   //return roundf(((value / 16384) * pow(2,(accRange + 1)))*100.0)/100.0;
   return (value / 16384.00) * pow(2, accRange);
 }
 
-double convertGyro(int16_t value) {
+double convertGyro(int16_t value)
+{
   return (value / 32768.00) * 250 * (gyroRange + 1);
 }
-
 
 void calculateAngles()
 {
   //Accelerometer angle calculations
   acc_total_vector = sqrt((ax * ax) + (ay * ay) + (az * az)); //Calculate the total accelerometer vector
   //57.296 = 1 / (3.142 / 180) The Arduino asin function is in radians
-  angle_pitch_acc = asin((float)ax / acc_total_vector) * 57.296;    //Calculate the pitch angle
-  angle_roll_acc = asin((float)ay / acc_total_vector) * -57.296;    //Calculate the roll angle
-  angle_yaw_acc = asin((float)az / acc_total_vector) * 57.296;    //Calculate the yaw angle
+  angle_pitch_acc = asin((float)ax / acc_total_vector) * 57.296; //Calculate the pitch angle
+  angle_roll_acc = asin((float)ay / acc_total_vector) * -57.296; //Calculate the roll angle
+  angle_yaw_acc = asin((float)az / acc_total_vector) * 57.296;   //Calculate the yaw angle
 
   angle_pitch = angle_pitch_acc;
   angle_roll = angle_roll_acc;
